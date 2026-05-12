d34e442af25bf84a789b738686f33cfa5a727ad3
Sophia <193290223+soap-phia@users.noreply.github.com>
2026-05-12 00:45:07 -0400

    broo

--- build.sh
+++ build.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-rm -rf dist/bin
-mkdir -p dist/bin
+rm -rf bin
+mkdir -p bin
 
 if [ ! -f "main.go" ]; then
-	echo "main.go not found."
-	exit 1
+    echo "main.go not found."
+    exit 1
 fi
 
 for os in linux darwin win32; do
-	if [ "$os" = "win32" ]; then
-		goos="windows"
-	else
-		goos=$os
-	fi
-	for arch in x64 arm64; do
-		if [ "$arch" = "x64" ]; then
-			goarch="amd64"
-		else
-			goarch=$arch
-		fi
-		if [ "$os" = "win32" ]; then
-			ext=".exe"
-		else
-			ext=""
-		fi
-		GOOS=$goos GOARCH=$goarch go build -o ./dist/bin/${os}-${arch}-mrrowisp${ext} main.go
-	done
+    if [ "$os" = "win32" ]; then
+        goos="windows"
+    else
+        goos=$os
+    fi
+    for arch in x64 arm64; do
+        if [ "$arch" = "x64" ]; then
+            goarch="amd64"
+        else
+            goarch=$arch
+        fi
+        if [ "$os" = "win32" ]; then
+            ext=".exe"
+        else
+            ext=""
+        fi
+        mkdir -p bin/${os}-${arch}
+        GOOS=$goos GOARCH=$goarch go build -o ./bin/${os}-${arch}/mrrowisp${ext} main.go
+    done
 done
 
-echo "Finished building. Binaries in ./dist/bin/"
+echo "Finished building. Binaries in ./bin/"
--- src/path.ts
+++ src/path.ts
@@ -1,10 +1,10 @@
 import * as os from "os";
 
-const arch = os.arch()
-const platform = os.platform()
+const arch = os.arch();
+const platform = os.platform();
+const ext = platform === "win32" ? ".exe" : "";
 
-const pkg = `${platform}-${arch}`
 const wispConfigPath = new URL("../dist/config.json", import.meta.url).pathname;
-const wispPath = new URL(`../bin/mrrowisp`, import.meta.url).pathname;
+const wispPath = new URL(`../bin/${platform}-${arch}/mrrowisp${ext}`, import.meta.url).pathname;
 
 export { wispConfigPath, wispPath };
