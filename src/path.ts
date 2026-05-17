ef0b945d17fea705a1fce539b073939a25a248c6
Sophia <193290223+soap-phia@users.noreply.github.com>
2026-05-16 21:59:14 -0400

    blegh

--- src/path.ts
+++ src/path.ts
@@ -1,8 +1,10 @@
 import * as path from "node:path";
 import * as os from "node:os";
+import { fileURLToPath } from "node:url";
 
 const bin = os.platform() === "win32" ? "mrrowisp.exe" : "mrrowisp";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const root = path.resolve(__dirname, "..");
 
 export const configPath = path.join(root, "dist", "config.json");
