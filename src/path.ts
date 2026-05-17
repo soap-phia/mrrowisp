5df5d80e54475eb3658b02a5fd787520dedb9e9f
Sophia <193290223+soap-phia@users.noreply.github.com>
2026-05-16 21:47:24 -0400

    a

--- src/path.ts
+++ src/path.ts
@@ -3,5 +3,5 @@ import * as os from "node:os";
 
 const bin = os.platform() === "win32" ? "mrrowisp.exe" : "mrrowisp";
 
-export const configPath = path.join(process.cwd(), "config.json");
-export const binPath = path.join(process.cwd(), "bin", bin);
+export const configPath = path.join("..", "dist", "config.json");
+export const binPath = path.join(".."   , "bin", bin);
